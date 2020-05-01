1f4ed24ba53d1b60693fd531808c4171af194702
david <user@DESKTOP-0T2RNIV>
2020-05-01 17:23:54 -0500

    Updates

---
+++ precache-manifest.1ec8a1273d2c32af4ea6e0ef2883b398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a853ca3395b4b3cbf75dc2ab1e8b170",
+    "url": "/react-gh-page/index.html"
+  },
+  {
+    "revision": "a579cf8863d826676f92",
+    "url": "/react-gh-page/static/css/main.070d0974.chunk.css"
+  },
+  {
+    "revision": "0c711fe742c0a1202652",
+    "url": "/react-gh-page/static/js/2.7ce231ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-page/static/js/2.7ce231ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a579cf8863d826676f92",
+    "url": "/react-gh-page/static/js/main.80e919b6.chunk.js"
+  },
+  {
+    "revision": "7a5ffd727f6985403811",
+    "url": "/react-gh-page/static/js/runtime-main.40e08315.js"
+  }
+]);
